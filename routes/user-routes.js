1ce7df7b568243128400470b860260006f6be8e8
Sheogorath9 <ogarrett189@gmail.com>
2023-02-04 10:50:52 -0500

    filled out user and thought routes

---
+++ routes/user-routes.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+
+const {
+    getAllUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    createUser,
+    deleteUser,
+
+  } = require('../controllers/user-controller');
+  
+  router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+  
+  router
+    .route('/:id')
+    .get(getUser)
+    .put(updateUser)
+    .delete(deleteUser);
+  
+  router
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+  
+  module.exports = router;
